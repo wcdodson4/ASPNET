3caaeeddb1a149a648b4d28f85e10eaa2e43fb55
William Dodson <wcdodson4@gmail.com>
2020-02-24 14:03:25 -0600

    view product method added

---
+++ ASPNET/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET.Models;
+
+namespace ASPNET
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+    }
+}
---
+++ ASPNET/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET.Models;
+using Dapper;
+
+namespace ASPNET
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return (Product)_conn.QuerySingle<Product>("SELECT * FROM Products WHERE ProductID = @id", new { id = id });
+        }
+    }
+}
